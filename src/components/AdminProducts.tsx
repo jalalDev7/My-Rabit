f6066978b725a0e936f8da7ee97fdaf2c847e76c
Mohamed Jalal <technochtak3@gmail.com>
2023-12-20 05:53:30 +0100

    main

--- src/components/AdminProducts.tsx
+++ src/components/AdminProducts.tsx
@@ -61,9 +61,7 @@ const AdminProducts = () => {
         },
       })
 
-      const handeEdit = (productId: string) => {
-        return redirect(`/admin?mode=edit&product=${productId}`)
-      }
+      
   return (
     <>
         <h1 className='text-2xl mt-10 px-5 mb-2'>
@@ -126,75 +124,6 @@ const AdminProducts = () => {
                     ): null}
                 </div>
             </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <h1 className='text-2xl mt-10 px-5 mb-2'>
-            All Products
-        </h1>
-        <div className='flex w-full flex-col items-center justify-center p-5 bg-white border-zinc-300 border-2 rounded-lg hover:shadow-xl'>
-            <input type="text" placeholder='Search by title' className='w-full border-zinc-200 border-2 rounded-lg p-2 my-1'
-            onChange={(event) => (handleQuery(event.currentTarget.value))}
-            />
-            {getProducts && getProducts.length > 0 ? (
-                getProducts.map((item) => {
-                    return (
-
-                        <div key={item.id} className='flex bg-zinc-50 rounded-lg shadow-md p-5 border-zinc-200 border-[1px] justify-between items-center w-full my-1' >
-                            <div className='flex text-lg font-semibold items-center justify-center'>
-                                {item.productTitle}
-                            </div>
-                            <div className='flex flex-row'>
-                            <Dialog open={isOpen} onOpenChange={(v) => {if(!v) {setIsOpen(v)}}} >
-                                <DialogTrigger onClick={() => {
-                                    setProductToEdit(item)
-                                    setIsOpen(true)}} asChild>
-                                    <AiOutlineEdit className=" text-4xl bg-blue-200 hover:bg-blue-300 rounded-lg p-1 m-1 " />
-                                </DialogTrigger>
-                                <DialogContent className='w-[70%]'>
-                                    <EditProduct productId={productToEdit}  />
-                                </DialogContent>
-                            </Dialog>
-
-                            <AiOutlineDelete className=" text-4xl bg-red-200  hover:bg-red-300 rounded-lg p-1 m-1 cursor-pointer " 
-                                onClick={() => (deleteProduct({id: item.id}))}
-                                />
-                            </div>
-                        </div>
-                    )
-                })
-            ): isLoading ? (
-                <div className='w-full mt-2 flex justify-center'>
-                    <div className='flex flex-col items-center gap-2'>
-                        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-                        <h3 className='font-semibold text-xl'>
-                        Getting Products from database
-                        </h3>
-                    </div>
-                </div>
-            ): isError ? (
-                <div className='w-full mt-2 flex justify-center'>
-                    <div className='flex flex-col items-center gap-2'>
-                        <h3 className='font-semibold text-xl'>
-                        0 Product found
-                        </h3>
-                    </div>
-                </div>
-            ): null}
-            
         </div>
     </>
   )
