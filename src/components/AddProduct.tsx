ba6cd712484a15e5feb94615c461a3087314ff3d
Mohamed Jalal <technochtak3@gmail.com>
2023-12-29 04:41:39 +0100

    main

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -252,7 +252,7 @@ const AddProduct = () => {
                                 <label htmlFor="dropezone-file" className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                         <p className="mb-2 text-md text-zinc-700 font-semibold">
-                                                Upload product file source ".zip"
+                                                Upload product file source .zip
                                             </p>
                                         <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
                                         <p className="mb-2 text-sm text-zinc-700">
