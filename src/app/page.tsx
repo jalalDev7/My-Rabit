5d4f249fc20e54fd413c2807c4d9cbe6b06c6bc1
Mohamed Jalal <technochtak3@gmail.com>
2024-01-03 01:57:51 +0100

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
   return (
     <>
     <NavBar/>
-    <MaxWidthWrapper className='mb-12 mt-12 sm:mt-40 flex flex-col items-center justify-center '>
-      <h1 className='max-w4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+    <MaxWidthWrapper className='mt-24 mb-24 lg:mt-24 2xl:mt-24 lg:mb-8 2xl:mb-8 flex flex-col items-center justify-center '>
+      <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl'>
         Get more <span className='text-blue-600'>Money</span> from your links.
       </h1>
-      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+      <p className='mt-5  text-zinc-700 sm:text-lg'>
         With My-Rabit you can earn more money from your traffic
       </p>
       <RegisterLink className={buttonVariants({
@@ -29,10 +29,10 @@ export default function Home() {
 
     <div>
       <div className='relative isolate'>
-        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-20 transform-gpu -z-10 overflow-hidden blur-3xl sm;-top-80'>
+        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-20 transform-gpu -z-10 overflow-hidden blur-3xl '>
           <div 
             style={{clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'}} 
-            className='relative left-[calc(50%-11rem)] aspect-[1115/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]' />
+            className='relative 2xl:left-[calc(70%-30rem)] 2xl:w-[72.1875rem] lg:left-[calc(70%-30rem)] lg:w-[72.1875rem] left-[calc(50%-11rem)] aspect-[1115/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 ' />
         </div>
         
         <div className='flex flex-col lg:flex-row 2xl:flex-row  w-full lg:py-24 2xl:py-24 py-4 lg:px-32 2xl:px-32 px-2 gap-14 items-center justify-center'>
@@ -62,10 +62,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm;-top-80'>
+        <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 top-[700px] transform-gpu -z-10 overflow-hidden blur-3xl '>
           <div 
             style={{clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'}} 
-            className='relative left-[calc(50%-13rem)] aspect-[1115/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]' />
+            className='relative left-[calc(50%-11rem)] w-[36.125rem] aspect-[1115/678]  -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]' />
         </div>
       </div>
     </div>
