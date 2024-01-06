50747529dcf3801b56eb5b248fbc3df971d9b079
Mohamed Jalal <technochtak3@gmail.com>
2024-01-06 21:10:12 +0100

    main

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -1,10 +1,9 @@
 import { trpc } from '@/app/_trpc/Client'
-import { Loader2 } from 'lucide-react'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import {AiOutlineDelete, AiOutlineEdit, AiFillEye} from "react-icons/ai"
 import UploadButton from './UploadButton'
-
+import { Skeleton } from "@/components/ui/skeleton"
 
 const linksList = () => {
   
@@ -53,22 +52,48 @@ const linksList = () => {
 })}
     </div> 
     ) : isLoading ? (
-      <div className='w-full mt-4 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Getting your links from database...
-        </h3>
-      </div>
-    </div>
-    ) : [
-      <div className='w-full mt-4 flex justify-start px-4'>
-
-        <h3 className='text-xl'>
-          There is no links right now, you can add more links.
-        </h3>
-    </div>
-    ]} 
+      <div className='grid lg:grid-cols-2 2xl:grid-cols-3 gap-4 p-4'>
+        <div  className='flex bg-zinc-50 rounded-lg shadow-md p-5 border-zinc-200 border-[1px] justify-between items-center  '>
+          <div className='flex flex-row items-center justify-center '>
+            <p className='flex flex-col border-r-[1px] border-zinc-300 text-sm font-bold pr-3 items-center justify-center'>
+              <Skeleton className='h-[20px] w-[20px] items-center cursor-pointer rounded-full' />
+            </p>
+            <p className='text-xl font-semibold content-stretch ml-3 sm:text-md '>
+            <Skeleton className='h-[40px] w-[200px] items-center cursor-pointer rounded-full' />
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <Skeleton className='h-[20px] w-[20px] items-center cursor-pointer rounded-full' />
+          </div>
+        </div>
+        <div  className='hidden lg:flex 2xl:flex bg-zinc-50 rounded-lg shadow-md p-5 border-zinc-200 border-[1px] justify-between items-center  '>
+          <div className='flex flex-row items-center justify-center '>
+            <p className='flex flex-col border-r-[1px] border-zinc-300 text-sm font-bold pr-3 items-center justify-center'>
+              <Skeleton className='h-[20px] w-[00px] items-center cursor-pointer rounded-full' />
+            </p>
+            <p className='text-xl font-semibold content-stretch ml-3 sm:text-md '>
+              <Skeleton className='h-[40px] w-[200px] items-center cursor-pointer rounded-full' />
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <Skeleton className='h-[20px] w-[20px] items-center cursor-pointer rounded-full' />
+          </div>
+        </div>
+        <div  className='hidden lg:flex 2xl:flex bg-zinc-50 rounded-lg shadow-md p-5 border-zinc-200 border-[1px] justify-between items-center  '>
+          <div className='flex flex-row items-center justify-center '>
+            <p className='flex flex-col border-r-[1px] border-zinc-300 text-sm font-bold pr-3 items-center justify-center'>
+              <Skeleton className='h-[20px] w-[20px] items-center cursor-pointer rounded-full' />
+            </p>
+            <p className='text-xl font-semibold content-stretch ml-3 sm:text-md '>
+              <Skeleton className='h-[40px] w-[200px] items-center cursor-pointer rounded-full' />
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-center'>
+            <Skeleton className='h-[20px] w-[20px] items-center cursor-pointer rounded-full' />
+          </div>
+        </div>
+      </div> 
+    ) : null} 
      </>           
   )
 }
